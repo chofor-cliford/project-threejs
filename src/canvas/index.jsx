668aca810c546df48cc6a507d55a2fec2eec32eb
Chofor <betterfuture032@gmail.com>
2023-12-26 22:32:54 -0600

    initial commit

---
+++ src/canvas/index.jsx
@@ -0,0 +1,33 @@
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center, Loader } from '@react-three/drei';
+
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+import { Suspense } from 'react';
+
+const CanvasModel = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      <CameraRig>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+    </Suspense>
+  )
+}
+
+export default CanvasModel
